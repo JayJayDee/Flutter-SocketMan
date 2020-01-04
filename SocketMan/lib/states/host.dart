9aae25057c37841648e4243080b8b654d568a049
JayJayDee <jindongp@gmail.com>
2020-01-04 15:49:38 +0900

    added host-action (test purpose)

--- SocketMan/lib/states/host.dart
+++ SocketMan/lib/states/host.dart
@@ -1,3 +1,4 @@
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:mobx/mobx.dart';
 
 part 'host.g.dart';
@@ -5,9 +6,29 @@ part 'host.g.dart';
 class Host = HostBase with _$Host;
 
 abstract class HostBase with Store {
+  IO.Socket _socket;
+
+  @observable
+  bool loading;
+
   @observable
   bool connected;
 
   @observable
   String host;
+
+  @action 
+  void connectViaWebsocket({ String host }) {
+    _socket = IO.io(host);
+    this.loading = true;
+
+    _socket.on('connect', (data) {
+      print('!!');
+      loading = false;
+    });
+    _socket.on('disconnect', (data) {
+      print('!');
+      loading = false;
+    });
+  }
 }
